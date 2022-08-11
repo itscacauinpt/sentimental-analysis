b137e52d852e89e3d12a9cacabe06fae297f05e0
cacau <anaclaudia.sza@outlook.com>
2022-08-10 23:36:16 -0300

    fix: exclui arquivos e comentados

--- AFINN/convert-afinn.js
+++ AFINN/convert-afinn.js
@@ -15,16 +15,3 @@ async function readAndWritefinn() {
 };
 
 readAndWritefinn();
-
-// const str = "abandon\t-2\nabandoned\t-2\nabandons\t-2\nabducted\t-2\nabduction\t-2\nabductions\t-2\nabhor\t-3\nabhorred\t-3\nabhorrent\t-3";
-// const strConverted = str.split('\n').map((file) => file.split(/\s+/));
-
-// const afinnObj = {};
-
-// for(let each of strConverted) {
-//   const [key, value] = each;
-//   afinnObj[key] = value;
-// };
-
-// console.log(afinnObj);
-
